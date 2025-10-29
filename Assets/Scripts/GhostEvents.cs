0b6c574b4563c275285e4fc6dbb1283eadfed697
Soarfin <Pdandersonblue@gmail.com>
2025-10-29 15:22:59 -0400

    Please don't fuck this up!!!!

---
+++ Assets/Scripts/GhostEvents.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostEvents : MonoBehaviour
+{
+    private LightFlicker lf;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        lf = GameObject.Find("Point Light").GetComponent<LightFlicker>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private void onEnterTrigger(Collider ollider)
+    {
+        if (ollider.CompareTag("Trigger"))
+        {
+
+
+        }
+
+    }
+}
